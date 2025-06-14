7fd53ce39cc34410cf63d2cf52a7af97a0fcb472
jesuscb123 <cbjesus@iesrafaelalberti.es>
2025-06-14 12:11:32 +0200

    Creando tareaDao

--- src/main/kotlin/data/Dao/TareaDao.kt
+++ src/main/kotlin/data/Dao/TareaDao.kt
@@ -4,5 +4,11 @@ import javax.sql.DataSource
 
 class TareaDao(private val datasource: DataSource) {
 
+    init{
+        val consulta = """
+                        CREATE TABLE IF NOT EXISTS TAREA (
+                            
+        """.trimIndent()
+    }
 
 }
--- src/main/kotlin/model/Tarea.kt
+++ src/main/kotlin/model/Tarea.kt
@@ -1,8 +1,12 @@
 package proyectoPorSemanaKotlin.dam.model
 
+import java.time.LocalDateTime
+
 class Tarea(val titulo: String,
             val descripcion: String,
             val fechaLimite: String) {
+
+    val inicioTarea = LocalDateTime.now().toString()
 
     var estado: Estado = Estado.PENDIENTE
         private set
